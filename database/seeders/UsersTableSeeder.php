429737ecd8feef1290b56def95ac1b1ff7acd261
茉妃 <>
2025-06-11 22:27:17 +0900

    更新

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,15 +5,21 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UsersTableSeede extends Seeder
+use App/Models/Users
+
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
+     public function run()
     {
-        //
+        User::create([
+            'name' => 'User1',
+            'email' => 'usertest@email.com',
+            'password' => bcrypt('abc123'),
+        ]);
     }
 }
