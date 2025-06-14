065866fa4cf997fa1e7631756a38784570de302b
茉妃 <>
2025-06-14 17:51:24 +0900

    修正

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use App/Models/Users
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
